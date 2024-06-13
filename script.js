46ded6938388f1c3e49c3553d16abadfe0b3d5cc
Reon1917 <linmyatphyo03@gmail.com>
2024-06-13 20:42:22 +0630

    Darkmode begins

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         copyrightText.classList.add('fade-in');
     }, 500); // Adjust the delay as needed
 });
+
+const checkbox = document.getElementById('checkbox');
+    checkbox.addEventListener('change', () => {
+        // change the theme of the website
+        document.body.classList.toggle('dark-theme');
+    });
